eac487da87302d00c3c8959edbf9313d75729401
Jas Chen <jaschen.tw@gmail.com>
2020-04-05 21:34:44 +0800

    Add editor config

--- src/createRef.js
+++ src/createRef.js
@@ -1,14 +1,13 @@
 export function createRef() {
-    if (!this.instanceId) {
-      this.instanceId = 0;
-    }
-  
-    const prefix = this.prefix || "";
-    const id = prefix + (this.instanceId++).toString(36);
-    const o = Object.create(null);
-    o.toString = () => id;
-    o.get = () => document.getElementById(id);
-  
-    return o;
+  if (!this.instanceId) {
+    this.instanceId = 0;
   }
-  
+
+  const prefix = this.prefix || "";
+  const id = prefix + (this.instanceId++).toString(36);
+  const o = Object.create(null);
+  o.toString = () => id;
+  o.get = () => document.getElementById(id);
+
+  return o;
+}
